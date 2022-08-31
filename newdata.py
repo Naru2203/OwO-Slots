79d618d6326b0176452a92445d1e3fc5a2737233
Naruv <101044276+Naru2203@users.noreply.github.com>
2022-08-31 20:55:59 +0700

    Add files via upload

---
+++ newdata.py
@@ -0,0 +1,66 @@
+import time
+import json
+import os
+from sys import exit
+def menu():
+ import json
+ file = open("settings.json", "r")
+ data = json.load(file)
+ file.close()
+ print('================================')
+ print(f'| [0] Exit And Save           |')
+ print(f'| [1] Change All Settings     |')
+ print(f'| [2] Change Token            |')
+ print(f'| [3] Change Channel          |')
+ print(f'| [4] Change Bet Amount       |')
+ print(f'| [5] Change Bet Rate         |')
+ print('================================')
+ choice = input("Enter Your Choice: ")
+ if choice == "0":
+  raise SystemExit
+ if choice == "1":
+  t(data,"True")
+  c(data,"True")
+  be(data,"True")
+ if choice == "2":
+  t(data,"False")
+ if choice == "3":
+  c(data,"False")
+ if choice == "4":
+  bet(data,"False")
+ if choice == "5":
+  r(data,"False")
+  
+def r(data,all):
+ data['rate'] = input("Please Enter Your Account Rate: ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
+    
+def t(data,all):
+ data['token'] = input("Please Enter Your Account Token: ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
+def c(data,all):
+ data['channel'] = input("Please Enter Your Channel ID: ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
+def bet(data,all):
+ data['bet'] = input("Enter Your Bet Amount (Must Be Integer): ")
+ file = open("settings.json", "w")
+ json.dump(data, file)
+ file.close()
+ print('Successfully saved!')
+ if not all == "True":
+  menu()
